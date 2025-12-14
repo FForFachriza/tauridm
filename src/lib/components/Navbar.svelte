e8030142e01d6f16f58c375f0beeb8921cfc77ae
FForFachriza <dimasavrian346@gmail.com>
2025-12-14 15:50:27 +0700

    feat (fe): - implement simple download logic - TODO: make the folder, pause, stop button work

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -3,6 +3,79 @@
 	import * as Dialog from '$lib/components/ui/dialog/index.js';
 	import { Button } from './ui/button';
 	import { Input } from './ui/input';
+	import { getAria2Connection } from '$lib/aria2_connection';
+	import { aria2 } from 'maria2';
+	import { toast } from 'svelte-sonner';
+	import { downloadState } from '$lib/store/store.svelte';
+
+	let dialogOpen = $state<boolean>(false);
+	let downloadUrl = $state<string>('');
+
+	const addDownload = async () => {
+		dialogOpen = false;
+		toast.info('Download added successfully');
+
+		try {
+			const conn = await getAria2Connection();
+			const gid = await aria2.addUri(conn, [downloadUrl]);
+
+			downloadState.push({
+				gid: gid,
+				filename: 'Fetching metadata...',
+				status: 'active',
+				progress: 0,
+				downloadSpeed: '0',
+				totalLength: '0',
+				completedLength: '0',
+				filepath: ''
+			});
+
+			const index = downloadState.findIndex((item) => item.gid === gid);
+
+			const intervalId = setInterval(async () => {
+				try {
+					const status = await aria2.tellStatus(conn, gid, [
+						'status',
+						'totalLength',
+						'completedLength',
+						'downloadSpeed',
+						'errorCode',
+						'files'
+					]);
+
+					if (index !== -1) {
+						const total = +status.totalLength;
+						const completed = +status.completedLength;
+						const progress = Number(total > 0 ? ((completed / total) * 100).toFixed(2) : '0');
+
+						if (downloadState[index].filename === 'Fetching metadata...' && status.files[0]?.path) {
+							const rawPath = status.files[0].path;
+							const cleanName = rawPath.replace(/\\/g, '/').split('/').pop() || 'Unknown File';
+
+							downloadState[index].filename = cleanName;
+							downloadState[index].filepath = rawPath;
+						}
+						downloadState[index].progress = progress;
+						downloadState[index].status = status.status;
+						downloadState[index].downloadSpeed = status.downloadSpeed;
+						downloadState[index].totalLength = status.totalLength;
+						downloadState[index].completedLength = status.completedLength;
+					}
+
+					if (['complete', 'error', 'removed'].includes(status.status)) {
+						if (status.status === 'complete') toast.success('Download Completed!');
+						if (status.status === 'error') toast.error(`Error: ${status.errorCode}`);
+						clearInterval(intervalId);
+					}
+				} catch (err) {
+					toast.error(`Error Pooling: ${err}`);
+					clearInterval(intervalId);
+				}
+			}, 1000);
+		} catch (err) {
+			toast.error(`Failed to Add Download: ${err}`);
+		}
+	};
 </script>
 
 <div class="flex flex-row justify-between p-4 border-b h-[61px]">
@@ -15,7 +88,7 @@
 		</div>
 	</div>
 
-	<Dialog.Root>
+	<Dialog.Root bind:open={dialogOpen}>
 		<Dialog.Trigger>
 			<Button class="flex flex-row justify-start w-full  items-center">
 				<PlusIcon />
@@ -26,10 +99,12 @@
 			<Dialog.Header>
 				<Dialog.Title>Add Download</Dialog.Title>
 			</Dialog.Header>
-			<Input placeholder="Paste Magnet or URL" type="text" />
+			<Input bind:value={downloadUrl} placeholder="Paste Magnet or URL" type="text" />
 			<div class="flex flex-row justify-end gap-x-4">
-				<Button variant="outline">Cancel</Button>
-				<Button>Download</Button>
+				<Dialog.Close>
+					<Button variant="outline">Cancel</Button>
+				</Dialog.Close>
+				<Button onclick={addDownload}>Download</Button>
 			</div>
 		</Dialog.Content>
 	</Dialog.Root>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,60 @@
-<script>
-	import Button from '$lib/components/ui/button/button.svelte';
+<script lang="ts">
+	import Badge from '$lib/components/ui/badge/badge.svelte';
+	import { Button } from '$lib/components/ui/button';
+	import * as Item from '$lib/components/ui/item/';
+	import { Progress } from '$lib/components/ui/progress';
+	import { downloadState } from '$lib/store/store.svelte';
+	import { FolderOpen, PauseIcon, SquareIcon, SquareStopIcon } from '@lucide/svelte';
+	import prettyBytes from 'pretty-bytes';
 </script>
 
-<main>
-	<Button>Hi</Button>
+<main class="w-full h-full gap-y-4 flex flex-col">
+	{#if downloadState.length === 0}
+		<div class="flex h-full w-full justify-center items-center">
+			<p>There is nothing here.....</p>
+		</div>
+	{:else}
+		{#each downloadState as item (item.gid)}
+			<Item.Root variant="outline">
+				<Item.Header class="flex justify-between items-center">
+					<p class="font-semibold text-foreground truncate flex-1">
+						{item.filename}
+					</p>
+					<Badge class="capitalize">{item.status}</Badge>
+				</Item.Header>
+				<Item.Media />
+				<Item.Content class="-mx-3.5 w-full">
+					<Progress value={item.progress} max={100} />
+					<Item.Description class="flex flex-row gap-x-4 mt-2">
+						<p>{prettyBytes(+item!.completedLength!)} / {prettyBytes(+item!.totalLength!)}</p>
+						<p>•</p>
+						<p>Speed: {prettyBytes(+item!.downloadSpeed!)}</p>
+						<p>•</p>
+						<p>
+							ETA: {Math.floor(
+								(+item!.totalLength! - +item!.completedLength!) / (+item!.downloadSpeed! || 1)
+							)} seconds
+						</p>
+					</Item.Description>
+				</Item.Content>
+				<Item.Footer class="flex flex-row gap-x-2 justify-start">
+					{#if item.status === 'complete'}
+						<Button variant="outline" class="flex flex-row justify-start items-center">
+							<FolderOpen />
+							<p>Open Folder</p>
+						</Button>
+					{:else}
+						<Button variant="outline" class="flex flex-row justify-start items-center">
+							<PauseIcon />
+							<p>Pause</p>
+						</Button>
+						<Button variant="outline" class="flex flex-row justify-start items-center">
+							<SquareStopIcon />
+							<p>Stop</p>
+						</Button>
+					{/if}
+				</Item.Footer>
+			</Item.Root>
+		{/each}
+	{/if}
 </main>
