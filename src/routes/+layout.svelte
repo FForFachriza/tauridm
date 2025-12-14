30a825dd8492ec8d03146a266a65edb6a0d97ba9
FForFachriza <dimasavrian346@gmail.com>
2025-12-14 15:48:22 +0700

    feat (fe): add sonner in layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,20 +1,12 @@
 <script lang="ts">
 	import './layout.css';
-	import { onMount } from 'svelte';
-	import { aria2, close } from 'maria2';
 	import Sidebar from '$lib/components/Sidebar.svelte';
-	import { getAria2Connection } from '$lib/aria2_connection';
+	import { Toaster } from '$lib/components/ui/sonner';
 
 	let { children } = $props();
-
-	onMount(async () => {
-		const conn = await getAria2Connection();
-		const version = await aria2.getVersion(conn);
-
-		console.log(version);
-	});
 </script>
 
+<Toaster richColors/>
 <Sidebar>
 	{@render children()}
 </Sidebar>
